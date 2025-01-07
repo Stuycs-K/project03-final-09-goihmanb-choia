d98e30ff26f5307d38de4da97af011afc3412d0b
Andrew Choi <achoi50@stuy.edu>
2025-01-07 14:39:37 -0500

    add headersto server.h

--- base_server.h
+++ base_server.h
@@ -1,4 +1,7 @@
 #ifndef BASE_SERVER.H
 #define BASE_SERVER.H
-
+void write_stats(int wol,char user[500]);
+int check_board(int board [][]);
+char * get_display(int board [][]);
+char * display_leaderboard (FILE * f);
 #endif
