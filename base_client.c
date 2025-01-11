d02a01f688f78ea62298d45b02929b54d853b2dc
Andrew Choi <achoi50@stuy.edu>
2025-01-11 18:45:23 -0500

    fix stashed changes to client

--- base_client.c
+++ base_client.c
@@ -1,37 +1,27 @@
 #include "pipe_networking.h"
 #include "game.h"
+#include "base_client.h"
 
 void game_loop(int to_server, int from_server, pid_t my_pid) {
   int board[3][3];
+  char display [1000];
   struct game_move move;
   if(read(from_server,&move,GS)<=0){
           printf("%s",strerror(errno));
   }
   if (move.ismove == YOUR_TURN) {
-    printf("Sending my move, first move\n");
-    char rowbuff [10];
-    char colbuff [10];
-    char *endptr;
-    printf("Enter the row, from 1 to 3: \n");
-    r = fgets(rowbuff, sizeof(r), stdin);
-    printf("Enter the column, from 1 to 3: \n");
-    c = fgets(colbuff, sizeof(c), stdin);
-    int r = strtol(str, &endptr, 10);
-    int c = strtol(str, &endptr, 10);
-    move.row = r;
-    move.col = c;
-    //FIX
-    board[r][c] = move.msg_type;
-    write(to_server, &move, GS);
+    write_to_server(move, board, to_server);
   }
   while(1) {
     if(read(from_server, &move, GS) < 0) {
       printf("%s\n", strerror(errno));
     }
+    board[move.row][move.col] = 3;
     printf("Received move from client, %d %d\n", move.row, move.col);
-    move.row = (move.row + 1) % 3;
-    move.col = (move.col + 1) % 3;
-    write(to_server, &move, GS);
+    char * d = format_brd(board, display);
+    printf("%s\n", d);
+    int moves[2] = write_to_server(move, board, to_server);
+    board[moves[0], moves[1]] = 3
     printf("Sending move to client, %d %d\n", move.row, move.col);
   }
 }
@@ -60,7 +50,6 @@ char * format_brd(int board[3][3], char ret[1000]){
             ret[index++]='-';
             ret[index++]='-';
             ret[index++]='-';
-            ret[index++]='-';
             ret[index++] = '\n';
 
         }
@@ -68,6 +57,24 @@ char * format_brd(int board[3][3], char ret[1000]){
             ret[index++] = '\n';
 
     return ret;
+}
+
+int * write_to_server(struct game_move move, int board[3][3], int to_server) {
+  char rowbuff [10];
+  char colbuff [10];
+  char *endptr;
+  printf("Enter the row, from 1 to 3: \n");
+  fgets(rowbuff, sizeof(rowbuff), stdin);
+  printf("Enter the column, from 1 to 3: \n");
+  fgets(colbuff, sizeof(colbuff), stdin);
+  int r = strtol(rowbuff, &endptr, 10);
+  int c = strtol(colbuff, &endptr, 10);
+  move.row = r;
+  move.col = c;
+  //FIX
+  // board[r][c] = move.msg_type;
+  write(to_server, &move, GS);
+  return [r, c];
 }
 int main() {
     int to_server;
